6367c8f4bcf418af88ffb4f4461b7fba9645b8da
yang <yueyangyu42@gmail.com>
2020-03-23 20:51:06 -0700

    Add loading animation

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,8 +1,52 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledLoading = styled.div``;
+const StyledLoading = styled.div`
+  display: inline-block;
+  position: relative;
+  top: 35vh;
+  width: 80px;
+  height: 80px;
+
+  div {
+    display: inline-block;
+    position: absolute;
+    left: 8px;
+    width: 16px;
+    background: black;
+    animation: loading 1.2s cubic-bezier(0, 0.5, 0.5, 1) infinite;
+  }
+  div:nth-child(1) {
+    left: 8px;
+    animation-delay: -0.24s;
+  }
+  div:nth-child(2) {
+    left: 32px;
+    animation-delay: -0.12s;
+  }
+  div:nth-child(3) {
+    left: 56px;
+    animation-delay: 0;
+  }
+  @keyframes loading {
+    0% {
+      top: 8px;
+      height: 64px;
+    }
+    50%,
+    100% {
+      top: 24px;
+      height: 32px;
+    }
+  }
+`;
 
 export const Loading = () => {
-  return <StyledLoading>{"Retrieving shoes.."}</StyledLoading>;
+  return (
+    <StyledLoading>
+      <div></div>
+      <div></div>
+      <div></div>
+    </StyledLoading>
+  );
 };
